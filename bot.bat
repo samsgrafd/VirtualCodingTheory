1f4a18f9ff4ac9e6dd3a322a557fc745c31c44fd
samsgrafd <samsgrafd@gmail.com>
2019-05-28 14:27:33 +0300

    bot bat
    uodate

--- bot.bat
+++ bot.bat
@@ -1,3 +1,3 @@
-python static/UserBin.py>>static/outBin.txt
-python static/UserBins.py>>static/outBins.txt
+python static/UserBin.py>static/outBin.txt
+python static/UserBins.py>static/outBins.txt
   
