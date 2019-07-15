7944a5245b226224cfd9af5ce1cb776f64f56fe3
samsgrafd <samsgrafd@gmail.com>
2019-07-15 18:10:20 +0300

    vcodetheory
    update

---
+++ vcodetheory.py
@@ -0,0 +1,32 @@
+Import os
+Import re
+Import sys
+
+Inall=""
+Inb="01"
+
+f = open("out.txt", 'r')
+
+Inall = f.read()
+
+f.close()
+
+print("this is inputStrBin:" + Inall)	
+
+
+
+def Agroup():
+
+	if  Inall="'011'":
+
+	
+	
+def function():
+ 	pass Bgroup():
+	if Inall="'01'"
+	if Inb in Inall:
+		aplusb()
+		
+	
+Def aplusb():
+	Agroup() + Bgroup()
